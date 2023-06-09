0776dc4361d6b97b4cd2fbaee13fd3a7442646f2
OfficeLab <74203530+OfficeLab@users.noreply.github.com>
2023-06-09 11:53:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# Library Import
+from flask import Flask, render_template, request
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+# DB Config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Message Data Object
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(500), nullable=True)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+# Routing
+@app.route("/<name>", methods=['GET', 'POST'])
+def start_pate(name):
+    if request.method == 'POST':
+            new_message = Message(
+                 user = name,
+                 content = request.form['content']
+            )
+            db.session.add(new_message)
+            db.session.commit()
+
+    messages = Message.query.order_by(Message.created_at).all()
+    return render_template('index.html', messages=messages, name=name)
+
+# Start Server python file with name x is started
+if __name__ == "__main__":
+    app.run(debug=True)
